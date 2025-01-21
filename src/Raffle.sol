95c10d6b48ec22c99a24644dbc505fa8653944d2
Sixtus Agbo <miracleagbosixtus@gmail.com>
2025-01-21 17:38:34 +0100

    Creating custom errors

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,24 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
@@ -8,13 +29,23 @@ pragma solidity 0.8.19;
  * @dev Implements Chainlink VRFv 2.5
  */
 contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent!"); //! Not very gas efficient because we're storing a giant string
+        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle()); //! Still not really gas efficient and it requires specific version of Solidity and specific compiler version
+        if (msg.value < i_entranceFee) {
+            //? Most gas efficient way to handle this
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+    }
 
     function pickWinner() public {}
 
